9b7a3da3321073e6960367fbbdc14eec710e0004
unknown <rafif.aja2004@gmail.com>
2026-08-31 13:08:58 +0700

    Create slite bar

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -24,6 +24,8 @@
                     </div>
                 @empty
                     <p class="text-center">Belum ada blog yang ditulis.</p>
+                    <p class="text-center">Belum ada blog yang ditulis.</p>
+                    <p class="text-center">Belum ada blog yang ditulis.</p>
                 @endforelse
                 <div class="d-flex justify-content-center mt-4">
                    {{ $blogs->links('pagination::bootstrap-5') }}
