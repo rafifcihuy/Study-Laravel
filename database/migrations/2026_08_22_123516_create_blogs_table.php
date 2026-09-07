8ca5d2d1d4dcd16701cf33d4ed43e5ee7bc8f944
unknown <rafif.aja2004@gmail.com>
2026-09-07 11:57:25 +0700

    fix bug and next revisi

--- database/migrations/2026_08_22_123516_create_blogs_table.php
+++ database/migrations/2026_08_22_123516_create_blogs_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
                 table: 'users',
                 indexName: 'blog_author_id'
             );
+            // erorr: SQLSTATE[HY000]: General error: 1215 Cannot add foreign key constraint (SQL: alter table `blogs` add constraint `blog_author_id` foreign key (`author_id`) references `users` (`id`))
             $table->foreignId('category_id')->constrained(
                 table: 'categories',
                 indexName: 'blog_category_id'
