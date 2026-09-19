1962fc0167cb0817a60b2a2319ec3fdbaefdcf5d
unknown <rafif.aja2004@gmail.com>
2026-09-19 19:50:23 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/about', function () {
     $judul = 'About';
     // $deskripsi = Blog::where('body', $body)->firstOrFail();
     // $deskripsi = Blog::where('body', '$body')->firstOrFail()
-    // $deskripsi = Blog::where('body', 'like', '%$body%')->firstOrFail(); tes
+    // $deskripsi = Blog::where('body', 'like', '%$body%')->firstOrFail(); 
     // $deskripsi = Blog::where('body', 'like', '%$body%')->firstOrFail(); 
     $deskripsi = Blog::all()->first();
     $nama = User::all()->first();
