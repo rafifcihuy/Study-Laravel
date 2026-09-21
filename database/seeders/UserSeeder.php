3dc7acaed4ebdb80d73b8851f3d667b66ff6a1a5
unknown <rafif.aja2004@gmail.com>
2026-09-21 20:09:16 +0700

    Study Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,6 +23,7 @@ class UserSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now(),
         ]);
+        // user tambahan
         User::factory()->count(10)->create();
     }
 }
